471b9623007eeef2e6fce32489836b054f45e554
Raja Haseeb <rajahaseeb147@gmail.com>
2022-11-01 16:13:28 +0900

    Update extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -1,18 +1,27 @@
 import cv2
 
 # Opens the Video file
-cap= cv2.VideoCapture('./tello_data/videos/structure_1.mp4')
-out_dir = './tello_data/frames/structure_1/'
+cap= cv2.VideoCapture('/home/pytholic/Desktop/Projects/uav_mapping/data/evo/videos/MAX_0040.MP4')
+length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+print( length )
+
+out_dir = '/home/pytholic/Desktop/Projects/uav_mapping/data/evo/MAX_0040/images/'
 idx=0
 while(cap.isOpened()):
     ret, frame = cap.read()
     idx += 1
-    if (idx % 1) == 0:
+    if (idx % 30) == 0:
         if ret == False:
             break
-        # flip for iphone
+        # for iphone
         #frame = cv2.flip(frame, -1)
+        #frame = cv2.resize(frame, (1080, 1920))  # width, height
+
+        # for evo
+        frame = cv2.resize(frame, (1920, 1080))
+
         cv2.imwrite(out_dir + f'{idx:06d}.jpg', frame)
+        print(f"Frame {idx} processed!")
 
 
 cap.release()
