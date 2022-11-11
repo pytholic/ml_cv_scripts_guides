c0a6981411edd5fc08cd97015e15e0192a9eee77
Raja Haseeb <rajahaseeb147@gmail.com>
2022-11-11 14:09:14 +0900

    Update extract_frames_ffmpeg.py

--- ffmpeg/extract_frames_ffmpeg.py
+++ ffmpeg/extract_frames_ffmpeg.py
@@ -2,23 +2,21 @@ import subprocess
 import argparse
 
 parser = argparse.ArgumentParser(
-                    prog = 'extract_frames',
-                    description = 'frame extraction pipeline')
+                    prog='extract_frames',
+                    description='frame extraction pipeline')
 
-parser.add_argument('-d', '--device', help='capture device name', choices=['iphone', 'evo', 'gopro'], default='iphone')
+parser.add_argument('-d', '--device', help='capture device name',
+                    choices=['iphone', 'evo', 'gopro'], default='iphone')
 parser.add_argument('-i', '--input', help='input file path')
 parser.add_argument('-o', '--output', help='output path')
 args = parser.parse_args()
 
-if args.device == 'evo':
-	size = '1920x1080'
-
 output = args.output + '/%06d.jpg'
 
-cmd = ['ffmpeg', 
-           '-i', args.input, 
-           '-s', size,
-           '-pix_fmt', 'yuvj444p',
-           output]
+if args.device == 'evo':
+    size = '1920x1080'
+    cmd = ['ffmpeg', '-i', args.input, '-s', size, '-pix_fmt', 'yuvj444p', output]
+else:
+    cmd = ['ffmpeg', '-i', args.input, '-pix_fmt', 'yuvj444p', output]
 
 subprocess.run(cmd)
